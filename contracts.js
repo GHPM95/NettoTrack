efac0e84803ef57a5fb774f5ee71ef02305165fd
GHPM95 <messinapaolo95@gmail.com>
2026-03-08 00:33:46 +0100

    Create contracts.js

---
+++ contracts.js
@@ -0,0 +1,18 @@
+const contractTypes = [
+
+"Tempo indeterminato",
+"Tempo determinato",
+"Apprendistato",
+"Contratto di somministrazione",
+"Contratto intermittente",
+"Contratto part-time",
+"Contratto full-time",
+"Collaborazione coordinata e continuativa",
+"Collaborazione occasionale",
+"Stage / tirocinio",
+"Lavoro autonomo",
+"Lavoro domestico",
+"Lavoro agricolo",
+"Lavoro a chiamata"
+
+]
