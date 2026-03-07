741c63f98cb070efb53c7d4734d7c52c6150f0f6
GHPM95 <messinapaolo95@gmail.com>
2026-03-08 00:32:35 +0100

    Create italy-geo-data.js

---
+++ italy-geo-data.js
@@ -0,0 +1,44 @@
+const italyGeoData = {
+
+regions:{
+
+Piemonte:{
+ provinces:{
+
+  Torino:[
+   ["Torino","10100"],
+   ["Moncalieri","10024"],
+   ["Nichelino","10042"],
+   ["Rivoli","10098"],
+   ["Collegno","10093"]
+  ],
+
+  Cuneo:[
+   ["Cuneo","12100"],
+   ["Alba","12051"],
+   ["Bra","12042"]
+  ]
+
+ }
+},
+
+Lombardia:{
+ provinces:{
+
+  Milano:[
+   ["Milano","20100"],
+   ["Sesto San Giovanni","20099"],
+   ["Rho","20017"]
+  ],
+
+  Bergamo:[
+   ["Bergamo","24100"],
+   ["Treviglio","24047"]
+  ]
+
+ }
+}
+
+}
+
+}
