71c346adeb0e99040347aa7f1cfecd004e0b3bb4
GHPM95 <messinapaolo95@gmail.com>
2026-03-08 00:34:28 +0100

    Create ccnl-list.js

---
+++ ccnl-list.js
@@ -0,0 +1,20 @@
+const ccnlList = [
+
+"Commercio",
+"Metalmeccanico industria",
+"Metalmeccanico artigianato",
+"Trasporti e logistica",
+"Sanità privata",
+"Sanità pubblica",
+"Edilizia industria",
+"Edilizia artigianato",
+"Turismo",
+"Pubblici esercizi",
+"Multiservizi",
+"Pulizie",
+"Grande distribuzione organizzata",
+"Scuola privata",
+"Banche",
+"Assicurazioni"
+
+]
